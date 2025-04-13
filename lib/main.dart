5ee4cc785f4f1745447fdb628dc7b113892c18b2
permpree-vornthaisong <permpreevornthaisong@gmail.com>
2025-04-14 01:37:16 +0700

    change name App

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,10 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:MusicPlaylistApp/page1.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:mp23/page1.dart';
+
 import 'package:path/path.dart' as path;
 import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
